9936ab547c794ba9d9e5f76430dae86d7e9176b7
EmiOnGitHub <pikachumybean@gmail.com>
2024-09-18 22:40:03 -0400

    Added static methods getSubTotal() and getTax() to BillCalculator class.

--- code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,5 +1,18 @@
 package edu.westga.cs1302.bill.model;
 
 public class BillCalculator {
-
+	
+	public static double getSubTotal(BillItem[] items) {
+		double subTotal = 0.0;
+		for (BillItem item : items) {
+			if (item != null) {
+				subTotal += item.getAmount();
+			}
+		}
+		return subTotal;
+	}
+	
+	public static double getTax(BillItem[] items) {
+		return BillCalculator.getSubTotal(items) * 0.1;
+	}
 }
--- code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetSubtotal.java
+++ code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetSubtotal.java
@@ -4,11 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.bill.model.Bill;
+import edu.westga.cs1302.bill.model.BillCalculator;
+import edu.westga.cs1302.bill.model.BillItem;
+
 class TestGetSubtotal {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetSubtotal() {
+		BillItem itemOne = new BillItem("Apple", 12.02);
+		BillItem itemTwo = new BillItem("Orange", 12.6676);
+		BillItem itemThree = new BillItem("Banana", 356.87);
+		Bill bill = new Bill();
+		bill.addItem(itemOne);
+		bill.addItem(itemTwo);
+		bill.addItem(itemThree);
+		double subtotal = BillCalculator.getSubTotal(bill.getItems());
+		assertEquals(381.5576, subtotal);
 	}
 
 }
