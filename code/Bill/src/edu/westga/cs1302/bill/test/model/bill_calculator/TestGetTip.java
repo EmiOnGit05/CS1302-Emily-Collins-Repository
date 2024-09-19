d6d73130c4ba513267b3e5b043435bded632b9ea
EmiOnGitHub <pikachumybean@gmail.com>
2024-09-18 23:03:49 -0400

    Added test cases to TestGetTip and TestGetTotal for BillCalculator class.

--- code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetTip.java
+++ code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetTip.java
@@ -4,11 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.bill.model.Bill;
+import edu.westga.cs1302.bill.model.BillCalculator;
+import edu.westga.cs1302.bill.model.BillItem;
+
 class TestGetTip {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetTip() {
+		BillItem itemOne = new BillItem("Apple", 12.02);
+		BillItem itemTwo = new BillItem("Orange", 12.6676);
+		BillItem itemThree = new BillItem("Banana", 356.87);
+		Bill bill = new Bill();
+		bill.addItem(itemOne);
+		bill.addItem(itemTwo);
+		bill.addItem(itemThree);
+		double tip = BillCalculator.getTip(bill.getItems());
+		assertEquals(76.31152, tip);
+	}
+	
+	@Test
+	void testGetTipOneItem() {
+		BillItem itemOne = new BillItem("Apple", 12.02);
+		Bill bill = new Bill();
+		bill.addItem(itemOne);
+		double tip = BillCalculator.getTip(bill.getItems());
+		assertEquals(2.404, tip);
+	}
+	
+	@Test
+	void testGetTipNoItems() {
+		Bill bill = new Bill();
+		double tip = BillCalculator.getTip(bill.getItems());
+		assertEquals(0.00, tip);
 	}
 
 }
--- code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetTotal.java
+++ code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetTotal.java
@@ -4,11 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.bill.model.Bill;
+import edu.westga.cs1302.bill.model.BillCalculator;
+import edu.westga.cs1302.bill.model.BillItem;
+
 class TestGetTotal {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetTotal() {
+		BillItem itemOne = new BillItem("Apple", 12.02);
+		BillItem itemTwo = new BillItem("Orange", 12.6676);
+		BillItem itemThree = new BillItem("Banana", 356.87);
+		Bill bill = new Bill();
+		bill.addItem(itemOne);
+		bill.addItem(itemTwo);
+		bill.addItem(itemThree);
+		double total = BillCalculator.getTotal(bill.getItems());
+		assertEquals(496.02487999999994, total);
+	}
+	
+	@Test
+	void testGetTotalOneItem() {
+		BillItem itemOne = new BillItem("Apple", 12.02);
+		Bill bill = new Bill();
+		bill.addItem(itemOne);
+		double total = BillCalculator.getTotal(bill.getItems());
+		assertEquals(15.626, total);
+	}
+	
+	@Test
+	void testGetTotalNoItems() {
+		Bill bill = new Bill();
+		double total = BillCalculator.getTotal(bill.getItems());
+		assertEquals(0.00, total);
 	}
 
 }
